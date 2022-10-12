938ab0707ae801b703e7dd1c0c2b1b3accd39f72
Pavanpatta <98875717+Pavanpatta@users.noreply.github.com>
2022-10-12 17:27:45 +0530

    n = int(input()) is_negative = 0 if n < 0:     is_negative = 1     n = -n rev = 0 while n > 0:     r = n % 10     rev = rev * 10 + r     n = n // 10 if is_negative == 1:     print(-rev) else:     print(rev)

---
+++ reverse_of_a_numbers.py
@@ -0,0 +1,14 @@
+n = int(input())
+is_negative = 0
+if n < 0:
+    is_negative = 1
+    n = -n
+rev = 0
+while n > 0:
+    r = n % 10
+    rev = rev * 10 + r
+    n = n // 10
+if is_negative == 1:
+    print(-rev)
+else:
+    print(rev)
